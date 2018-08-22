99b9330995bcc2224c60a9d7507efa489d3a9a1f
Damir Šerfezi <damir.serfezi@gmail.com>
2018-08-22 22:52:01 +0200

    sayYo function added

--- main.cpp
+++ main.cpp
@@ -6,15 +6,23 @@ int showBit()
 	return a;
 }
 
+void sayYo()
+{
+	cout << "Yo";
+}
+
 int main()
 {
 	cout << "Hello";
 	cout << " world";
 	
 	cout << showBit();
+	
 	bool c, d;
 	c = true;
 	d = false;
+	
+	sayYo();
 	
 	return 0;
 }
